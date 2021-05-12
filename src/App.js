3ff3fbe1a4f5dfcf74d9e1620a1bc7a7afdc9c98
Rodolfo Santana <rdmoedas@gmail.com>
2021-05-11 22:04:15 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Card from './components/Card'
+
+function App() {
+  return (
+    <div>
+      Conteúdo
+
+      <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Card/index.js
@@ -0,0 +1,11 @@
+import './card.css'
+
+function Card() {
+    return (
+        <div className="card">
+            Sou um Card
+        </div>
+    )
+}
+
+export default Card;
